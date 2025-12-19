fe7e8a7ee064b3aed04d1ee2a6c294081dfba0d0
Your Name <your@email.com>
2025-12-19 14:58:32 +0700

    Add jsx files

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Greetings from './Greetings'
+import User from './user'
+
+function App() {
+  return (
+    <div className="card">
+      <Greetings />
+      <User name="Swan Yee Htet" />
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Greetings.jsx
@@ -0,0 +1,6 @@
+function Greetings() {
+    return <h3>Hello, React!</h3>
+  }
+  
+  export default Greetings
+  
